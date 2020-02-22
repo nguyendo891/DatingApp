fcc3ff8cb8bba98b292254e839cec0027238c578
dodinhnguyen <nguyendo891@yahoo.com>
2020-02-22 23:14:55 +0100

    add member-detail,member-list,member-card components ,user, photo models, user service,member-detail,member-list resolver, using ngx gallery from @nomadreservations/ngx-gallery

---
+++ DatingApp-SPA/src/app/_models/photo.ts
@@ -0,0 +1,7 @@
+export interface Photo {
+  ud: number;
+  url: string;
+  description: string;
+  dateAdded: Date;
+  isMain: boolean;
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,10 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
+import {
+  BrowserModule
+  // HammerGestureConfig,
+  // HAMMER_GESTURE_CONFIG
+} from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TabsModule } from "ngx-bootstrap/tabs";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
+import { JwtModule } from "@auth0/angular-jwt";
+import { NgxGalleryModule } from "@nomadreservations/ngx-gallery";
+//this "ngx-gallery" is used Renderer which is nolonger supported by Angular 9
+//use "@nomadreservations/ngx-gallery" instead, it is used Renderer2
+//import { NgxGalleryModule } from "ngx-gallery";
 
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
@@ -13,10 +23,29 @@ import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 import { from } from "rxjs";
-import { MemberListComponent } from "./member-list/member-list.component";
+import { MemberListComponent } from "./members/member-list/member-list.component";
 import { ListsComponent } from "./lists/lists.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { appRoutes } from "./routes";
+import { MemberCardComponent } from "./members/member-card/member-card.component";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
+import { AlertifyService } from "./_services/alertify.service";
+import { AuthGuard } from "./_guards/auth.guard";
+import { UserService } from "./_services/user.service";
+import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
+import { MemberListResolver } from "./_resolvers/member-list.resolver";
+
+export function tokenGetter() {
+  return localStorage.getItem("token");
+}
+
+//this fix the error of hammerJs for swiping left and right in angular 8
+// export class CustomHammerConfig extends HammerGestureConfig {
+//   overrides = {
+//     pinch: { enable: false },
+//     rotate: { enable: false }
+//   };
+// }
 
 @NgModule({
   declarations: [
@@ -26,7 +55,9 @@ import { appRoutes } from "./routes";
     RegisterComponent,
     MemberListComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -34,9 +65,28 @@ import { appRoutes } from "./routes";
     FormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    TabsModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ["localhost:5000"],
+        blacklistedRoutes: ["localhost:5000/api/auth"]
+      }
+    })
   ],
-  providers: [AuthService, ErrorInterceptorProvider],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider,
+    AlertifyService,
+    AuthGuard,
+    UserService,
+    MemberDetailResolver,
+    MemberListResolver
+    //this fix the error of hammerJs for swiping left and right in angular 8
+    // { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
